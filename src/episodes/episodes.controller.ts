29a646b959f7a19803fc35759871635e04d2c758
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2026-07-24 02:55:02 +0530

    7: guards

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -8,13 +8,16 @@ import {
   ParseIntPipe,
   Post,
   Query,
+  UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateEpisodeDto } from './dto/create-episode.dto';
 import { EpisodesService } from './episodes.service';
 import { ConfigService } from '../config/config.service';
 import { IsPositivePipe } from '../pipes/is-positive-pipe';
+import { ApiKeyGuard } from '../guards/api-key.guard';
 
+@UseGuards(ApiKeyGuard)
 @Controller('episodes')
 export class EpisodesController {
   constructor(
