560a5bc75f4a76aabebb56fc958c4c3a4e0385dd
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2026-07-24 01:13:16 +0530

    4: dependency injection

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,28 +1,32 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { CreateEpisodeDto } from './dto/create-episode.dto';
+import { EpisodesService } from './episodes.service';
+import { ConfigService } from '../config/config.service';
 
 @Controller('episodes')
 export class EpisodesController {
+  constructor(
+    private episodesService: EpisodesService,
+    private configService: ConfigService,
+  ) {}
+
   @Get()
   findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc') {
-    console.log(sort);
-    return 'all episodes';
+    return this.episodesService.findAll(sort);
   }
 
   @Get('featured')
   findFeatured() {
-    return 'featured episodes';
+    return this.episodesService.findFeatured();
   }
 
   @Get(':id')
   findOne(@Param() id: string) {
-    console.log(id);
-    return 'episode by id';
+    return this.episodesService.findOne(id);
   }
 
   @Post()
   create(@Body() dto: CreateEpisodeDto) {
-    console.log(dto);
-    return 'created new episode';
+    return this.episodesService.create(dto);
   }
 }
